f279b5a9c0a113e15f2b317e7a039cf3f7176be5
Hossain <arafat.kd99@gmail.com>
2023-12-22 18:27:51 +0100

    Add  correct_name method to base Decorator

--- base_decorator.rb
+++ base_decorator.rb
@@ -4,4 +4,10 @@ class BaseDecorator < Nameable
   def initialize(nameable)
     @nameable = nameable
   end
+
+# Implement correct_name method
+  def correct_name
+    @nameable.correct_name
+  end
+
 end
