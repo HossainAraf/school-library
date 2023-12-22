82e87d3471c1e23de62b071d7149be99523a83aa
Hossain <arafat.kd99@gmail.com>
2023-12-22 18:38:28 +0100

    Add class for capitalize decoratot

--- base_decorator.rb
+++ base_decorator.rb
@@ -2,12 +2,12 @@ require_relative 'nameable'
 
 class BaseDecorator < Nameable
   def initialize(nameable)
+    super()
     @nameable = nameable
   end
 
-# Implement correct_name method
+  # Implement correct_name method
   def correct_name
     @nameable.correct_name
   end
-
 end
