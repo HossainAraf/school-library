363dab5308951ddd5dd8bac05cb29fa90037c426
Hossain <arafat.kd99@gmail.com>
2024-01-12 15:58:28 +0100

    Inherit book and person to Rental class

--- rental.rb
+++ rental.rb
@@ -1,3 +1,6 @@
+require_relative 'book'
+require_relative 'person'
+
 class Rental
   attr_accessor :date, :book, :person
 
@@ -5,7 +8,7 @@ class Rental
     @date = date
     @book = book
     @person = person
-    book.rentels << self
-    person.rentels << self
+    book.rentals << self
+    person.rentals << self
   end
 end
